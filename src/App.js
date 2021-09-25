0c8e014f60fe007b6e1ac0df2cd645b1e817d4e6
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-09-25 22:20:59 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,69 +5,7 @@ import Album from "./components/Album";
  import AddAlbum from './components/AddAlbum'
 
 function App() {
-  const [album] = useState({
-    album_title: "Rockstar",
-    artist: "A.R.Rahaman",
-    album_cover:
-      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-  });
-  const [albums] = useState([
-    {
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    },
-    {
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    },
-    { 
-      id:1,
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    },
-    {
-       id:2,
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    
-    },
-    {
-       id:3,
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    },
-    {
-       id:4,
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    },
-    {
-       id:6,
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    },
-    {
-       id:6,
-      album_title: "Rockstar",
-      artist: "A.R.Rahaman",
-      album_cover:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ceb0a114960774.56037a57f1752.jpg",
-    }
-  ]);
+ 
 
   return (
     <div className="App">
